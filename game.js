ae1ffccbd6b82e367e310cc8a6338b4458b72c63
Unknown <nicoo.oliver@gmail.com>
2017-12-19 21:36:59 +0100

    Base du projet -> le fond

---
+++ game.js
@@ -0,0 +1,22 @@
+var cs = document.getElementById("cv");
+var ctx = cs.getContext("2d");
+
+var drawGround = function ()
+{
+	for(let j=0;j<7;j++)
+    {
+        for(let i=0;i<6;i++)
+        {
+            ctx.drawImage(ground,0,0,256,256,100*i,100*j,100,100);
+        }
+    }
+}
+
+var drawZomb = function () 
+{
+	context.drawImage(zombies,sx,sy,32,32,x,y,64,64);
+}
+var ground = new Image();
+ground.src="ground.png";
+ground.onload = drawGround;
+
