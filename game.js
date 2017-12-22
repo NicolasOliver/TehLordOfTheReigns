9b6ff8ef7aaabf912b9a93414066666d93ed18c2
Unknown <nicoo.oliver@gmail.com>
2017-12-22 22:42:09 +0100

    Descente auto d'un gobelin avec position de départ aléatoire sur 100 px

--- game.js
+++ game.js
@@ -1,11 +1,27 @@
 var cs = document.getElementById("cv");
 var ctx = cs.getContext("2d");
 
+// Ensemble des variables utiles
+var ground = new Image();
+ground.src="ground.png";
+var goblin = new Image();
+goblin.src="goblin.png";
+soundManager.url='soundmanager2.swf';
+soundManager.debugMode=false;
+
+// Variables de coordonnées
+var sx = 0;
+var sy = 0;
+var x = Math.round(Math.random()*601);
+var y = Math.round(Math.random()*101);
+
+// Fonction qui joue une musique au lancement de la page
 var play = function () {
     soundManager.createSound('son','shallnotpass.wav');
     soundManager.play('son');
 }
 
+// Fonction qui dessine le plateau de jeu
 var drawGround = function ()
 {
 	for(let j=0;j<8;j++)
@@ -17,6 +33,13 @@ var drawGround = function ()
     }
 }
 
+// Fonction qui dessine les zombies de premier niveau
+var drawGoblin = function ()
+{
+	ctx.drawImage(goblin,sx,sy,32,32,x,y,32,32);
+}
+
+// Fonction pour faire suivre un élement au déplacement de la souris
 function follow(evenement)
 {
     if(navigator.appName=="Microsoft Internet Explorer")
@@ -33,13 +56,39 @@ function follow(evenement)
     }
 }
 
-var ground = new Image();
-ground.src="ground.png";
-ground.onload = drawGround;
+// Fonction qui cadence le déplacement des zombies vers le bas
+function autodown()
+{
+	setInterval(function () {
+		sx=sx+32;
+		if(sx==96)
+		{
+			sx=0;
+		}
+		y=y+5;
+		if(y>768)
+		{
+			ctx.clearRect(0,0,600,800);
+			drawGround();
+		}
+		
+		else
+		{
+			ctx.clearRect(0,0,600,800);
+			drawGround();
+			drawGoblin();
+		}
+	},100)
+}
 
-soundManager.url='soundmanager2.swf';
-soundManager.debugMode=false;
-soundManager.onload = play;
+// Fonction principale du jeu
+function game()
+{
+	drawGround();
+	play();
+	document.onmousemove = follow;
+	autodown();
+}
 
+window.onload = game;
 
-document.onmousemove = follow;
